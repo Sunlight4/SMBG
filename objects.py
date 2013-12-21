8ff5f7e386e24e4e65e93bc37cb025354ad9d75c
Ian Huang <ianhuang@tookland.home>
2013-12-21 14:54:27 -0500

    Your code… My github is glitching.

--- objects.py
+++ objects.py
@@ -2,8 +2,12 @@ import pygame
 
 class Object(pygame.sprite.Sprite):
     def __init__(self,pos,img):
-        self.img = pygame.image.load(img)
+        self.image = pygame.image.load(img)
         self.pos = pos
     def render(self,surf):
+<<<<<<< HEAD
         surf.blit(self.img,self.pos)
+=======
+        surf.blit(self.image,self.pos)
     
+>>>>>>> df0745527897a3018769313ce8b25e1e3c374dc9
