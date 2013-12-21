6c28a4a75cd4792d85ba95b2459d06156f54f435
Ian Huang <ianhuang@tookland.home>
2013-12-21 14:56:56 -0500

    room.py

--- Room.py
+++ Room.py
@@ -1,5 +1,10 @@
 import pygame
 
 class Room:
-    def __init__(self,objs,bg):
-        pass # you do the rest. :D
+    def __init__(self,objs,bg): # objs includes tiles
+        self.objs = objs
+        self.bg = bg
+    def update(self,surf):
+        for obj in objs:
+            obj.render(surf)
+        
