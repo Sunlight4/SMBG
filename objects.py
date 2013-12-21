df0745527897a3018769313ce8b25e1e3c374dc9
Sunlight4 <ethan.saff@gmail.com>
2013-12-21 14:49:23 -0500

    Attribute Rename

--- objects.py
+++ objects.py
@@ -2,8 +2,8 @@ import pygame
 
 class Object(pygame.sprite.Sprite):
     def __init__(self,pos,img):
-        self.img = pygame.image.load(img)
+        self.image = pygame.image.load(img)
         self.pos = pos
     def render(self,surf):
-        surf.blit(self.img,self.pos)
+        surf.blit(self.image,self.pos)
     
