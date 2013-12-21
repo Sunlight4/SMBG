2c5de8a3514957fcd73602e30729b9c6a6247c0a
Ian Huang <ianhuang@tookland.home>
2013-12-21 15:04:35 -0500

    More functions for room.py
    Added init, and more attributes.

--- Room.py
+++ Room.py
@@ -1,10 +1,18 @@
 import pygame
 
 class Room:
-    def __init__(self,objs,bg): # objs includes tiles
+    def __init__(self,objs,bg,bgmusic): # objs includes tiles
         self.objs = objs
         self.bg = bg
+        self.bgmusic = bgmusic
     def update(self,surf):
         for obj in objs:
             obj.render(surf)
-        
+    def init(self,surf): # init for initalize. func initalizes the room
+        for obj in objs:
+            obj.render(surf)
+
+        pygame.mixer.music.load(bgmusic)
+        music.set_volume(1)
+        pygame.mixer.music.play(-1)
+            
